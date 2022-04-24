d80e526721ef1f92825692a3c74c133e018fcd05
n3r0-b1n4ry <tiendungfc89@gmail.com>
2022-04-24 21:28:51 +0700

    Update Code

--- backend/app.py
+++ backend/app.py
@@ -26,40 +26,40 @@ def submitdata ():
     qr = genQR(os.path.join(TMPPATH, '{}'.format(h.hexdigest())))
 
     if data['type'] == 'text':
-        tmp = base64.b64decode(data['data']['content']).decode("utf16")
+        tmp = base64.b64decode(data['data']['content']).decode("latin_1")
         result = qr.genText(data=tmp)
     elif data['type'] == 'url':
-        tmp = base64.b64decode(data['data']['content']).decode("utf16")
+        tmp = base64.b64decode(data['data']['content']).decode("latin_1")
         if 'http://' not in tmp or 'https://' not in tmp:
             tmp = 'http://'+ tmp
         result = qr.genURL(data=tmp)
     elif data['type'] == 'email':
         tmp = {
-            'email':base64.b64decode(data['data']['email']).decode(),
-            'subject':base64.b64decode(data['data']['subject']).decode(),
-            'mess':base64.b64decode(data['data']['mess']).decode()
+            'email':base64.b64decode(data['data']['email']).decode('latin_1'),
+            'subject':base64.b64decode(data['data']['subject']).decode('latin_1'),
+            'mess':base64.b64decode(data['data']['mess']).decode('latin_1')
         }
         result = qr.genEmail(email=tmp['email'], subject=tmp['subject'], mess=tmp['mess'])
     elif data['type'] == 'wifi':
         tmp = {
-            'network_name':base64.b64decode(data['data']['network_name']).decode(),
-            'password':base64.b64decode(data['data']['password']).decode(),
-            'encryption':base64.b64decode(data['data']['encryption']).decode()
+            'network_name':base64.b64decode(data['data']['network_name']).decode('latin_1'),
+            'password':base64.b64decode(data['data']['password']).decode('latin_1'),
+            'encryption':base64.b64decode(data['data']['encryption']).decode('latin_1')
         }
         result = qr.genWifi(ssid=tmp['network_name'],password=tmp['password'],type=tmp['encryption'])
     elif data['type'] == 'vcard':
         tmp = {
-            'firstname':base64.b64decode(data['data']['fname']).decode(),
-            'lastname':base64.b64decode(data['data']['lname']).decode(),
-            'fullname':"{} {}".format(base64.b64decode(data['data']['lname']).decode(), base64.b64decode(data['data']['fname']).decode()),
-            'organize':base64.b64decode(data['data']['company_ch']).decode(),
-            'addr':"{} {}".format(base64.b64decode(data['data']['street']).decode(), base64.b64decode(data['data']['state']).decode()),
-            'city':base64.b64decode(data['data']['city']).decode(),
-            'taxcode':base64.b64decode(data['data']['zip']).decode(),
-            'country':base64.b64decode(data['data']['country']).decode(),
-            'phonenumber':base64.b64decode(data['data']['mobile']).decode(),
-            'email':base64.b64decode(data['data']['email']).decode(),
-            'website':base64.b64decode(data['data']['website']).decode()
+            'firstname':base64.b64decode(data['data']['fname']).decode('latin_1'),
+            'lastname':base64.b64decode(data['data']['lname']).decode('latin_1'),
+            'fullname':"{} {}".format(base64.b64decode(data['data']['lname']).decode('latin_1'), base64.b64decode(data['data']['fname']).decode('latin_1')),
+            'organize':base64.b64decode(data['data']['company_ch']).decode('latin_1'),
+            'addr':"{} {}".format(base64.b64decode(data['data']['street']).decode('latin_1'), base64.b64decode(data['data']['state']).decode('latin_1')),
+            'city':base64.b64decode(data['data']['city']).decode('latin_1'),
+            'taxcode':base64.b64decode(data['data']['zip']).decode('latin_1'),
+            'country':base64.b64decode(data['data']['country']).decode('latin_1'),
+            'phonenumber':base64.b64decode(data['data']['mobile']).decode('latin_1'),
+            'email':base64.b64decode(data['data']['email']).decode('latin_1'),
+            'website':base64.b64decode(data['data']['website']).decode('latin_1')
         }
         result = qr.genVcard(data=tmp)
     # elif data['type'] == 'text':
